2a18b0b48b714eff7d474e1bf7bd1b7a8c839df1
JulianaRkg544 <julianna.rkg544@gmail.com>
2022-07-24 16:47:38 -0300

    rota de clientes finalizada

--- src/controllers/clientController.js
+++ src/controllers/clientController.js
@@ -18,3 +18,51 @@ export async function addClient(req, res, next){
   }
 
 }
+
+export async function listClient(req, res){
+   try {
+    const clientList = await db.query("SELECT * FROM customers")
+    res.status(200).send(clientList.rows)
+   } catch (error) {
+    console.log(error)
+    return res.sendStatus(500)
+   }
+}
+
+export async function listClientById (req, res){
+    const id = req.params.id
+    
+    try {
+        const clientById = await db.query(`SELECT * FROM customers WHERE id=$1`,[id])
+        if(clientById.rows.length==0){
+            console.log("client not found")
+            return res.sendStatus(404)
+        }
+        res.status(200).send(clientById.rows)
+        
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(500)
+    }
+}
+
+export async function updateClient(req, res){
+    try {
+        //inserir cliente no db pelo id
+        const id = req.body.id
+        const client = req.body
+
+        const existentCpf = await db.query("SELECT FROM customers WHERE cpf = $1", [client.cpf])
+    if (existentCpf.rows.length>0){
+        console.log("cpf ja cadastrado")
+        return res.status(409).send("cpf already in")
+    }
+        const updatedClient = await db.query("UPDATE customers SET (name=$2 phone=$3 cpf=$4 birthday=$5) WHERE id=$1", 
+        [id, client.name, client.phone, client.cpf, client.birthday ] )
+        res.send(201).status(updatedClient.rows)
+
+    } catch (error) {
+        console.log(error)
+        return res.sendStatus(500)
+    }
+}
--- src/routes/clientRoute.js
+++ src/routes/clientRoute.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { addClient } from "../controllers/clientController.js";
+import { addClient, listClient, listClientById, updateClient } from "../controllers/clientController.js";
 import { clientValidation } from "../middlewares/clientMiddleware.js";
 
 const clientRouter = Router()
 
 clientRouter.post("/customers", clientValidation ,addClient)
-// clientRouter.get("/customers", listClient)
-// clientRouter.put("/customers", updateClient)
+clientRouter.get("/customers", listClient)
+clientRouter.get("/customers/:id", listClientById)
+clientRouter.put("/customers/:id",clientValidation ,updateClient)
 
 export default clientRouter
